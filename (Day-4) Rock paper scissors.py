dccc120169bab47175865ee35ef82e788babcec7
Hagar <108490885+ChaoticMaximoff@users.noreply.github.com>
2023-03-15 16:03:08 +0200

    day 4 edit

--- (Day-4) Rock paper scissors.py
+++ (Day-4) Rock paper scissors.py
@@ -32,21 +32,22 @@ scissors = '''
 game = [rock, paper, scissors]
 
 user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user >= 0 and user <= 2:
-  print(game[user])
-
-computer = random.randint(0, 2)
-print("Computer chose:\n" + game[computer] + "\n")
-
-# rock -> sci  0 -> 2
-# sci -> pap   2 -> 1
-# pap -> rock  1 -> 0
 if user < 0 or user > 2:
   print("You entered an invalid number. You lose.\n")
-elif user == computer:
-  print("It's a draw.\n")
-elif (user == 0 and computer == 2) or (user == 2 and computer == 1) or (user == 1 and computer == 0):
-  print("You win!\n")
-else :
-  print("You lose.\n")
+else:
+  print(game[user])
+  computer = random.randint(0, 2)
+  print("Computer chose:\n" + game[computer] + "\n")
+
+  # rock -> sci  0 -> 2
+  # sci -> pap   2 -> 1
+  # pap -> rock  1 -> 0
+
+  if user == computer:
+    print("It's a draw.\n")
+  elif (user == 0 and computer == 2) or (user == 2 and computer == 1) or (user == 1 and computer == 0):
+    print("You win!\n")
+  else :
+    print("You lose.\n")
+
   
